4a19b1d56acfe9dd9c16e7a814c537bea8904620
gabriel <gabrieldausque.pro@gmail.com>
2020-04-08 13:43:17 +0200

    Fix issues on serialization/deserialization from elasticsearch + synchronisation implementation : round 3

--- CanopeeAgent/Canopee.StandardLibrary/Configuration/AspNet/AspNetCanopeeServerConfigurationReader.cs
+++ CanopeeAgent/Canopee.StandardLibrary/Configuration/AspNet/AspNetCanopeeServerConfigurationReader.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Composition;
 using System.Net.Http;
+using System.Text.Json;
 using Canopee.Common;
 using Canopee.Common.Configuration.AspNet.Dtos;
 using Canopee.Common.Logging;
@@ -26,21 +27,41 @@ namespace Canopee.StandardLibrary.Configuration.AspNet
         public void Initialize(IConfiguration serviceConfiguration, IConfiguration loggingConfiguration)
         {
             Logger = CanopeeLoggerFactory.Instance().GetLogger(loggingConfiguration, this.GetType());
+            if (bool.Parse(serviceConfiguration["NoSSLCheck"]))
+            {
+                var handler = new HttpClientHandler();
+                handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+                handler.ServerCertificateCustomValidationCallback = (message, certificate2, arg3, arg4) => true;
+                _httpClient = new HttpClient(handler);
+            }
         }
 
         public ICollection<AgentGroupDto> GetGroups(string agentId)
         {
+            var agentGroups = new List<AgentGroupDto>(); 
             try
             {
                 var url =
-                    $"{ConfigurationService.Instance.GetCanopeeConfiguration()["Configuration:url"]}?agentId={agentId}";
+                    $"{ConfigurationService.Instance.GetCanopeeConfiguration()["Configuration:url"]}/api/AgentGroup?agentId={agentId}";
                 var response = _httpClient.GetAsync(url).Result;
+                var resultAsString = response.Content.ReadAsStringAsync().Result;
+                var jsonObjects = JsonSerializer.Deserialize<List<JsonObject>>(resultAsString);
+                foreach (var jsonObj in jsonObjects)
+                {
+                    var cleanObject = JsonObject.CleanDocument(jsonObj);
+                    agentGroups.Add(new AgentGroupDto()
+                    {
+                        AgentId = cleanObject.GetProperty<string>("agentId"),
+                        Group = cleanObject.GetProperty<string>("group"),
+                        Priority = cleanObject.GetProperty<long>("priority")
+                    });
+                }
             }
             catch (Exception ex)
             {
                 Logger?.LogError($"Error while getting groups : {ex}");
             }
-            return new List<AgentGroupDto>();
+            return agentGroups;
         }
 
         public JsonObject GetConfiguration(string agentId, string @group)
